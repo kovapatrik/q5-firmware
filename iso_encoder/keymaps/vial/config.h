cbc15d072f6bb1d45e850bd025c242ff810562f7
kovapatrik <kovapatrik@gmail.com>
2024-08-09 22:51:04 +0200

    fix: update some files

--- iso_encoder/keymaps/vial/config.h
+++ iso_encoder/keymaps/vial/config.h
@@ -4,5 +4,7 @@
 
 #define VIAL_KEYBOARD_UID {0x1F, 0x7A, 0x73, 0x45, 0x5A, 0x86, 0x8C, 0x56}
 
+#define DIP_SWITCH_MATRIX_GRID { { 5, 4 }}
+
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
